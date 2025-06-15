b52e7db5f3f7a25075f9a3a2638c47ecb2e3b781
Alonso Rojas <alonso.rojas@alumnos.ucn.cl>
2025-06-15 11:22:03 -0400

    feat: create ProductDialog modal for product detail

---
+++ src/components/products/ProductDialog.tsx
@@ -0,0 +1,30 @@
+import { Product } from "@/interfaces/Product";
+import { Dialog, DialogContent, DialogTitle, DialogDescription, DialogHeader} from "@/components/ui/dialog";
+import Image from 'next/image';
+
+interface ProductDialogProps {
+    product: Product | null;
+    open: boolean;
+    onClose: () => void;
+}
+
+export const ProductDialog = ({ product, open, onClose }: ProductDialogProps) => {
+    if (!product) return null; 
+
+    return (
+        <Dialog open={open} onOpenChange={onClose}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>{product.name}</DialogTitle>
+                    <DialogDescription>Detalle del Producto</DialogDescription>
+                </DialogHeader>         
+                <div className="flex flex-col items-center">
+                    <Image src= "/Producto.png" alt={product.name} width={200} height={200} className="object-contain mb-4" />
+                    <p className = "text-blue-700 font-bold text-2xl mt-4">{product.price.toLocaleString("es-CL", {style: "currency", currency: "CLP", minimumFractionDigits: 0, maximumFractionDigits: 2,})}</p>
+                    <p className = "text-gray-600 mt-2">{product.description ?? "Sin descripción"} </p>
+
+                </div>
+            </DialogContent>
+        </Dialog>
+    )
+};
