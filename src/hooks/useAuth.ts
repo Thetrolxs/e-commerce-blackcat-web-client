c05c2ce05a8aad58a768072ff3ea85c560cd5459
Thetrolxs <moralesharnisch@gmail.com>
2025-06-26 10:05:21 -0400

    chore!: delete file from client, lib, service.

--- src/hooks/useAuth.ts
+++
@@ -1,6 +0,0 @@
-import { useAuthContext } from '@/contexts/authContext';
-
-export const useAuth = () => {
-  const { user, login, logout } = useAuthContext();
-  return { user, login, logout };
-};
