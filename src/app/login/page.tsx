04e67b3b1a14c213af1deffe43dc2dafeb4296a4
Thetrolxs <moralesharnisch@gmail.com>
2025-06-28 18:21:53 -0400

    fix: export LoginForm in login route

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,11 @@
-import LoginPage from "@/views/LoginView";
+import { LoginForm } from "@/components/shared/LoginForm";
 
 export default function Login() {
-  return <LoginPage />;
+  return (
+    <div className="flex flex-col min-h-screen bg-white dark:bg-black">
+      <main className="flex-1 flex items-center justify-center p-4">
+        <LoginForm />
+      </main>
+    </div>
+  );
 }
