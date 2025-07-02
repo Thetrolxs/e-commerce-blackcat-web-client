b60f8a046c844062af7e8e389aa24b5d39898c9e
Thetrolxs <moralesharnisch@gmail.com>
2025-07-02 12:01:52 -0400

    feat: add RegisterForm

---
+++ src/components/auth/RegisterForm.tsx
@@ -0,0 +1,280 @@
+"use client";
+
+import {
+  RegisterRequest,
+  registerSchema,
+} from "../../interfaces/RegisterRequest";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useAuthContext } from "@/contexts/authContext";
+import Link from "next/link";
+import { Button } from "../ui/Button";
+import { Card, CardHeader, CardTitle, CardContent } from "../ui/card";
+import {
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+  Form,
+} from "../ui/form";
+import { Input } from "../ui/input";
+
+export const RegisterForm = () => {
+  const form = useForm<RegisterRequest>({
+    resolver: zodResolver(registerSchema),
+    defaultValues: {
+      firtsName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      thelephone: "",
+      birthDate: "",
+      street: "",
+      number: "",
+      commune: "",
+      region: "",
+    },
+  });
+
+  const { register } = useAuthContext();
+
+  const onSubmit = async (data: RegisterRequest) => {
+    await register(data);
+  };
+
+  return (
+    <Card className="w-full max-w-md p-6 bg-[#D9D9D9] dark:bg-gray-800 shadow-lg rounded-lg">
+      <CardHeader className="text-center space-y-1">
+        <CardTitle className="text-2xl font-bold text-gray-900 dark:text-gray-50">
+          Registrarse
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              control={form.control}
+              name="firtsName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nombre</FormLabel>
+                  <FormControl>
+                    <Input
+                      className="bg-white"
+                      placeholder="Nombre"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="lastName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Apellido</FormLabel>
+                  <FormControl>
+                    <Input
+                      className="bg-white"
+                      placeholder="Apellido"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Correo electrónico</FormLabel>
+                  <FormControl>
+                    <Input
+                      className="bg-white"
+                      type="email"
+                      placeholder="tu@ejemplo.com"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="thelephone"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Teléfono</FormLabel>
+                  <FormControl>
+                    <Input
+                      className="bg-white"
+                      type="tel"
+                      placeholder="+51234567890"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="birthDate"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Fecha de nacimiento</FormLabel>
+                  <FormControl>
+                    <Input className="bg-white" type="date" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="street"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Calle</FormLabel>
+                  <FormControl>
+                    <Input
+                      className="bg-white"
+                      type="text"
+                      placeholder="(opcional)"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="number"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Número</FormLabel>
+                  <FormControl>
+                    <Input
+                      className="bg-white"
+                      type="number"
+                      placeholder="(opcional)"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="commune"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Comuna</FormLabel>
+                  <FormControl>
+                    <Input
+                      className="bg-white"
+                      type="text"
+                      placeholder="(opcional)"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="region"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Region</FormLabel>
+                  <FormControl>
+                    <Input
+                      className="bg-white"
+                      type="text"
+                      placeholder="(opcional)"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Contraseña</FormLabel>
+                  <FormControl>
+                    <Input
+                      className="bg-white"
+                      type="password"
+                      placeholder="********"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirmar contraseña</FormLabel>
+                  <FormControl>
+                    <Input
+                      className="bg-white"
+                      type="password"
+                      placeholder="********"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button
+              type="submit"
+              className="w-full bg-[#404040] hover:bg-[#505050] text-white"
+            >
+              Registrarse
+            </Button>
+          </form>
+        </Form>
+
+        <div className="mt-6 text-center text-sm">
+          <span className="text-black-600 dark:text-black-300">
+            ¿Ya tienes una cuenta?{" "}
+          </span>
+          <Link
+            href="/login"
+            className="font-semibold text-gray-800 hover:underline dark:text-gray-100"
+          >
+            Inicia sesión
+          </Link>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
