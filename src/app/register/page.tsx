74b69a67393d6d6b36f80fe6b6a9ce3d876609cd
Thetrolxs <moralesharnisch@gmail.com>
2025-07-02 12:02:07 -0400

    feat: add registerForm page

---
+++ src/app/register/page.tsx
@@ -0,0 +1,11 @@
+import { RegisterForm } from "@/components/auth/RegisterForm";
+
+export default function Login() {
+  return (
+    <div className="flex flex-col min-h-screen bg-white dark:bg-black">
+      <main className="flex-1 flex items-center justify-center p-4">
+        <RegisterForm />
+      </main>
+    </div>
+  );
+}
