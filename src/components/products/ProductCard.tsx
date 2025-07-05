b3c2d272880874614d39aba5fd71c748bff1e761
Alonso Rojas <alonso.rojas@alumnos.ucn.cl>
2025-07-04 21:30:01 -0400

    feat: add quantity selector and add-to-cart logic with toast

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -1,5 +1,9 @@
+import { useAuthContext } from "@/contexts/authContext";
 import { Product } from "@/interfaces/Product";
 import Image from "next/image";
+import { useCart } from "@/contexts/cartContext";
+import {useState} from "react";
+import { toast } from "sonner";
 
 interface ProductCardProps {
   product: Product;
@@ -11,6 +15,11 @@ export const ProductCard = ({ product, onClick }: ProductCardProps) => {
     Array.isArray(product.urls) && product.urls.length > 0
       ? product.urls[0]
       : "/Producto.png";
+
+  const { user } = useAuthContext();
+  const { addToCart } = useCart();
+  const [quantity,setQuantity] = useState(1);
+
   return (
     <div className="bg-gray-100 rounded-lg shadow p-4 w-full flex flex-col items-center text-sm min-h-[100px] hover:shadow-lg transition">
       {/* Imagen */}
@@ -45,17 +54,42 @@ export const ProductCard = ({ product, onClick }: ProductCardProps) => {
 
       {/* Controles */}
       <div className="flex items-center justify-center gap-2 mt-1">
-        <button className="cursor-pointer bg-gray-600 text-white px-2 py-0.5 rounded-full hover:bg-gray-800 transition">
+        <button
+          onClick={() => setQuantity(prev => Math.max(1, prev - 1))}
+          className="cursor-pointer bg-gray-600 text-white px-2 py-0.5 rounded-full hover:bg-gray-800 transition"
+        >
           -
         </button>
-        <span className="bg-white px-3 py-1 rounded border select-none">1</span>
-        <button className="cursor-pointer bg-gray-600 text-white px-2 py-1 rounded-full hover:bg-gray-800 transition">
+        <span className="bg-white px-3 py-1 rounded border select-none">{quantity}</span>
+        <button
+          onClick={() => setQuantity(prev => prev + 1)}
+          className="cursor-pointer bg-gray-600 text-white px-2 py-1 rounded-full hover:bg-gray-800 transition"
+        >
           +
         </button>
       </div>
 
+
       {/* Botón Agregar */}
-      <button className="cursor-pointer mt-3 bg-black text-white text-sm px-6 py-2 rounded-full hover:bg-gray-800 transition">
+      <button
+        onClick={() => {
+          if (!product.stock || product.stock < quantity) {
+            toast.error(`No hay stock disponible para ${product.name}`, {
+              position: "top-right",
+              duration: 3000,
+            });
+            return;
+          }
+          for (let i = 0; i < quantity; i++) {
+            addToCart(product);
+          }
+          toast.success(`${product.name} añadido al carrito 🛒`, {
+          position: "top-right",
+          duration: 2000, 
+       });
+        }}
+        className="cursor-pointer mt-3 bg-black text-white text-sm px-6 py-2 rounded-full hover:bg-gray-800 transition"
+      >
         Agregar
       </button>
     </div>
